e24f4963b9af3d0ee3fbeec03da8caff3a6cef75
Vanja Buturac <contact@vanjavk.me>
2021-07-17 21:55:38 +0200

    Dodavanje item show #2 (nedovrseno)

--- app/src/main/java/me/vanjavk/isa_shows_app_vanjavk/model/Show.kt
+++ app/src/main/java/me/vanjavk/isa_shows_app_vanjavk/model/Show.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 
 data class Show(
     val ID: String,
-    val name: String,
+    val title: String,
     val description: String,
     val genre: List<Genre>,
     @DrawableRes val imageResourceId: Int
