6f44993b7af6a33a613bfb02adb0a50858805629
vanjavk <contact@vanjavk.me>
2021-07-19 00:52:44 +0200

    review adapter

--- app/src/main/java/me/vanjavk/isa_shows_app_vanjavk/ShowDetailsActivity.kt
+++ app/src/main/java/me/vanjavk/isa_shows_app_vanjavk/ShowDetailsActivity.kt
@@ -5,7 +5,10 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import me.vanjavk.isa_shows_app_vanjavk.databinding.ActivityShowDetailsBinding
 import me.vanjavk.isa_shows_app_vanjavk.model.Show
 
@@ -20,6 +23,11 @@ class ShowsDetailsActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityShowDetailsBinding
+
+    private var reviewsAdapter: ReviewsAdapter? = null
+
+    private lateinit var show: Show
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,7 +38,7 @@ class ShowsDetailsActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
         val ID = intent.extras?.get(EXTRA_ID)
-        val show = shows.find { it.ID == ID }!!
+        show = shows.find { it.ID == ID }!!
 
         binding.toolbarLayout.title = show.title
 
@@ -39,6 +47,32 @@ class ShowsDetailsActivity : AppCompatActivity() {
         binding.showDescription.text = show.description
 
         setContentView(binding.root)
+
+        initReviewsRecycler()
+    }
+
+    private fun initReviewsRecycler() {
+
+        reviewsAdapter = ReviewsAdapter(emptyList())
+
+        binding.reviewsRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.reviewsRecyclerView.adapter = reviewsAdapter
+        if (show.reviews.isEmpty()) {
+            binding.reviewsRecyclerView.visibility = GONE
+            binding.showReviewRating.visibility = GONE
+            binding.showRatingBar.visibility = GONE
+            binding.noReviewsYet.visibility = VISIBLE
+        } else {
+            val averageRating = show.reviews.sumOf { it.stars } / show.reviews.count().toFloat()
+            binding.reviewsRecyclerView.visibility = VISIBLE
+            binding.showReviewRating.text =
+                "${show.reviews.count()} REVIEWS, ${averageRating} AVERAGE"
+            binding.showReviewRating.visibility = VISIBLE
+            binding.showRatingBar.rating = averageRating
+            binding.showRatingBar.visibility = VISIBLE
+            binding.noReviewsYet.visibility = GONE
+            reviewsAdapter?.setItems(show.reviews)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
