c758c436004e570c2dcac75e7543250b371d5e55
vanjavk <contact@vanjavk.me>
2021-07-20 18:42:28 +0200

    ViewModels (nedovrseno)

---
+++ app/src/main/java/me/vanjavk/isa_shows_app_vanjavk/viewmodel/ShowDetailsViewModel.kt
@@ -0,0 +1,40 @@
+package me.vanjavk.isa_shows_app_vanjavk.viewmodel
+
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import me.vanjavk.isa_shows_app_vanjavk.R
+import me.vanjavk.isa_shows_app_vanjavk.model.Genre
+import me.vanjavk.isa_shows_app_vanjavk.model.Review
+import me.vanjavk.isa_shows_app_vanjavk.model.Show
+
+
+class ShowDetailsViewModel : ViewModel() {
+
+    private lateinit var show: Show
+
+    private val showsViewModel: ShowsViewModel by viewModels()
+
+    private val showLiveData: MutableLiveData<Show> by lazy {
+        MutableLiveData<Show>()
+    }
+
+    fun getShowLiveData(): LiveData<Show> {
+        return showLiveData
+    }
+
+    fun initShow(id: String) {
+
+        show = ShowsViewModel.getShowsLiveData().value?.find { it.ID == id }!!
+
+        showLiveData.value = show
+    }
+
+    fun addReview(review: Review) {
+        show.reviews.add(review)
+        showLiveData.value = show
+    }
+
+
+}
